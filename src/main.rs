f0cc9936f1f9c4c69ea3b0695fdea23485e20cc0
AJTJ <janke.aaron@gmail.com>
2021-08-02 17:50:13 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let my_u8: u8 = 0b0001_0010;
+    let my_i8 = 0b1000_0111_u8 as i8;
+    let neg_23 = 0b11101001_u8 as i8;
+
+    println!("{}", my_u8);
+    println!("{}", my_i8);
+    println!("{}", neg_23);
+}
